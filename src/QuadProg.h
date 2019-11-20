06a4b94025971442413f86843bd1c522e5cb8d33
Stéphane Caron <stephane.caron@lirmm.fr>
2019-11-20 13:50:25 +0900

    [doc] Document class attributes and solve() functions

--- src/QuadProg.h
+++ src/QuadProg.h
@@ -36,7 +36,9 @@ extern "C" int qpgen2_(double* dmat, double* dvec, const int* fddmat,
 	const int* fdamat, const int* q, const int* meq, int* iact, int* nact,
 	int* iter, double* work, const int* ierr);
 
-
+/** Common method for Quadprog solver classes.
+ *
+ */
 class QuadProgCommon
 {
 public:
@@ -56,15 +58,23 @@ protected:
 		bool isDecomp);
 
 protected:
-	MatrixXd Q_;
-	VectorXd C_, B_, X_;
-	int fail_;
-	VectorXi iact_;
-	VectorXi iter_;
-	VectorXd work_;
+	MatrixXd Q_; /**< Cost function matrix. \note WILL BE DESTROYED ON EXIT */
+	VectorXd C_; /**< Cost function vector. \note WILL BE DESTROYED ON EXIT */
+    VectorXd B_; /**< Inequality constraint vector. \note ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE CHANGED SIGNS ON EXIT */
+    VectorXd X_; /**< Solution of the quadratic program */
+	int fail_; /**< Failure code, 0 is success */
+	VectorXi iact_; /**< Vector of constraints that are active in solution */
+    VectorXi iter_; /**< 2x1 vector, first component gives the number of "main"
+                      iterations, the second one says how many constraints were
+                      deleted after they became active */
+    VectorXd work_; /**< Working space vector with length at least
+                      \f$2n+r(r+5)/2+2q+1\f$ where \f$r=\min(n,q)\f$ */
 };
 
 
+/** Dense quadratic program.
+ *
+ */
 class QuadProgDense : public QuadProgCommon
 {
 public:
@@ -73,16 +83,42 @@ public:
 
 	EIGEN_QUADPROG_API void problem(int nrvar, int nreq, int nrineq);
 
+    /** Solve quadratic program.
+     *
+     * \param Q Cost function matrix.
+     *
+     * \param C Cost function vector.
+     *
+     * \param Aeq Matrix of equality constraints \f$A_{eq} x = b_{eq}\f$.
+     *
+     * \param Beq Vector of equality constraints \f$A_{eq} x = b_{eq}\f$.
+     *
+     * \param Aineq Matrix of equality constraints \f$A_{ineq} x \leq b_{ineq}\f$.
+     *
+     * \param Bineq Vector of equality constraints \f$A_{ineq} x \leq b_{ineq}\f$.
+     *
+     * \param isDecomp If false (default), Q is not decomposed. If true, the
+     * solver will assume that Q is already decomposed into \f$Q = R^T R\f$ and
+     * it is given \f$R^{-1}\f$.
+     *
+     * \note Both Q and C will be destroyed on exit. Also, entries
+     * corresponding to equality constraints in Aeq and Beq may have changed
+     * signs on exit.
+     *
+     */
 	EIGEN_QUADPROG_API bool solve(const MatrixXd& Q, const VectorXd& C,
 		const MatrixXd& Aeq, const VectorXd& Beq,
 		const MatrixXd& Aineq, const VectorXd& Bineq,
 		bool isDecomp=false);
 
 private:
-	MatrixXd A_;
+	MatrixXd A_; /**< Inequality constraint matrix. \note ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE CHANGED SIGNES ON EXIT */
 };
 
 
+/** Sparse quadratic program.
+ *
+ */
 class QuadProgSparse : public QuadProgCommon
 {
 public:
@@ -91,14 +127,45 @@ public:
 
 	EIGEN_QUADPROG_API void problem(int nrvar, int nreq, int nrineq);
 
+    /** Solve quadratic program.
+     *
+     * \param Q Cost function matrix.
+     *
+     * \param C Cost function vector.
+     *
+     * \param Aeq Matrix of equality constraints \f$A_{eq} x = b_{eq}\f$.
+     *
+     * \param Beq Vector of equality constraints \f$A_{eq} x = b_{eq}\f$.
+     *
+     * \param Aineq Matrix of equality constraints \f$A_{ineq} x \leq b_{ineq}\f$.
+     *
+     * \param Bineq Vector of equality constraints \f$A_{ineq} x \leq b_{ineq}\f$.
+     *
+     * \param isDecomp If false (default), Q is not decomposed. If true, the
+     * solver will assume that Q is already decomposed into \f$Q = R^T R\f$ and
+     * it is given \f$R^{-1}\f$.
+     *
+     * \note Both Q and C will be destroyed on exit. Also, entries
+     * corresponding to equality constraints in Aeq and Beq may have changed
+     * signs on exit.
+     *
+     */
 	EIGEN_QUADPROG_API bool solve(const MatrixXd& Q, const VectorXd& C,
 		const SparseMatrix<double>& Aeq, const VectorXd& Beq,
 		const SparseMatrix<double>& Aineq, const VectorXd& Bineq,
 		bool isDecomp=false);
 
 private:
-	MatrixXd A_;
-	MatrixXi iA_;
+	MatrixXd A_; /**< Inequality constraint matrix. \note ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE CHANGED SIGNS ON EXIT */
+    MatrixXi iA_; /**< Inequality constraint matrix: these two matrices store
+                    the matrix A in compact form. the format is: 
+                    \f[ 
+                      A = [A1 A2]
+                    \f]
+           - iA_(1,i) is the number of non-zero elements in column i of A
+           - iA_(k,i) for k>=2, is equal to j if the (k-1)-th non-zero element in column i of A is A(i,j)
+           - A_(k,i) for k>=1, is equal to the k-th non-zero element
+                      in column i of A. */
 };
 
 } // namespace Eigen
