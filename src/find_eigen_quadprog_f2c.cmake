3baa2068a1a24f7d00c53181491f8f9dbbb3b3c0
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-02-20 12:38:59 +0100

    [pre-commit] Apply fixes

--- src/find_eigen_quadprog_f2c.cmake
+++ src/find_eigen_quadprog_f2c.cmake
@@ -9,12 +9,14 @@ function(find_eigen_quadprog_f2c)
   find_path(
     F2C_INCLUDE_DIR
     NAMES f2c.h
-    HINTS ${F2C_PREFIX})
+    HINTS ${F2C_PREFIX}
+  )
 
   find_library(
     F2C_LIBRARY
     NAMES f2c
-    PATHS ${F2C_PREFIX})
+    PATHS ${F2C_PREFIX}
+  )
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(F2C DEFAULT_MSG F2C_LIBRARY F2C_INCLUDE_DIR)
@@ -22,8 +24,8 @@ function(find_eigen_quadprog_f2c)
   if(F2C_FOUND)
     add_library(eigen-quadprog::f2c INTERFACE IMPORTED GLOBAL)
     set_target_properties(
-      eigen-quadprog::f2c
-      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${F2C_INCLUDE_DIR}
-                 INTERFACE_LINK_LIBRARIES ${F2C_LIBRARY})
+      eigen-quadprog::f2c PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${F2C_INCLUDE_DIR}
+                                     INTERFACE_LINK_LIBRARIES ${F2C_LIBRARY}
+    )
   endif()
 endfunction()
